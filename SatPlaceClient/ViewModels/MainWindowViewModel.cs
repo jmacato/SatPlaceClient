dd0e3019cf0c5286fb1cc654172e534393ab7451
Jumar Macato <hikari.netto23@gmail.com>
2020-03-03 11:52:15 +0800

    add locks for connection ready prop

--- SatPlaceClient/ViewModels/MainWindowViewModel.cs
+++ SatPlaceClient/ViewModels/MainWindowViewModel.cs
@@ -17,34 +17,65 @@ namespace SatPlaceClient.ViewModels
 
         private SocketIO SatPlaceClient { get; }
 
+        private object _connectionReadyLock = new object();
+        private bool _connectionReady;
+
+        public bool ConnectionReady
+        {
+            get
+            {
+                lock (_connectionReadyLock)
+                    return _connectionReady;
+            }
+            set
+            {
+                lock (_connectionReadyLock)
+                    this.RaiseAndSetIfChanged(ref _connectionReady, value, nameof(ConnectionReady));
+            }
+        }
+
         public MainWindowViewModel()
         {
-             SatPlaceClient = new SocketIO("https://api.satoshis.place"); 
+            SatPlaceClient = new SocketIO("https://api.satoshis.place");
+
+            SetupHandlers();
+
 
             OpenImageDialog = ReactiveCommand.CreateFromTask(async () =>
             {
-                var client = new SocketIO("https://api.satoshis.place");
-
-                client.OnConnected += async delegate
-                {
-                    client.On("GET_LATEST_PIXELS_RESULT", res =>
-                    {
-                        Console.WriteLine(res.Text);
-                    });
-
-                    client.On("BROADCAST_STATS", res =>
-                    {
-                        Console.WriteLine(res.Text);
-                    });
-
-                    await client.EmitAsync("GET_LATEST_PIXELS", null);
-
-                };
-
-
-
-                await client.ConnectAsync();
+                if (ConnectionReady)
+                    await SatPlaceClient.EmitAsync("GET_LATEST_PIXELS", null);
             });
+        }
+
+        private async void SetupHandlers()
+        {
+            await SatPlaceClient.ConnectAsync();
+
+            SatPlaceClient.OnConnected += delegate
+            {
+                ConnectionReady = true;
+
+                SatPlaceClient.On("GET_LATEST_PIXELS_RESULT", res =>
+                {
+                    Console.WriteLine(res.Text);
+                });
+
+                SatPlaceClient.On("BROADCAST_STATS", res =>
+                {
+                    Console.WriteLine(res.Text);
+                });
+            };
+
+            SatPlaceClient.OnClosed += delegate
+            {
+                ConnectionReady = false;
+            };
+
+            SatPlaceClient.OnError += delegate
+            {
+                ConnectionReady = false;
+            };
         }
     }
 }

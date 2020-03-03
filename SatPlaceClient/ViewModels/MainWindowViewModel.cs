886e7c45b6ed4d30c41b205a4be97be5835b250e
Jumar Macato <hikari.netto23@gmail.com>
2020-03-03 11:25:14 +0800

    Add initial socket.io code

---
+++ SatPlaceClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.WebSockets;
+using System.Reactive;
+using System.Text;
+using ReactiveUI;
+using SocketIOClient;
+
+namespace SatPlaceClient.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string OpenImageDialogText { get; set; } = "Open Image";
+
+        public ReactiveCommand<Unit, Unit> OpenImageDialog { get; }
+
+        public MainWindowViewModel()
+        {
+            OpenImageDialog = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var client = new SocketIO("https://api.satoshis.place");
+
+                client.OnConnected += async delegate
+                {
+                    client.On("GET_LATEST_PIXELS_RESULT", res =>
+                    {
+                        Console.WriteLine(res.Text);
+                    });
+
+                    client.On("BROADCAST_STATS", res =>
+                    {
+                        Console.WriteLine(res.Text);
+                    });
+
+                    await client.EmitAsync("GET_LATEST_PIXELS", null);
+
+                };
+
+                await client.ConnectAsync();
+            });
+        }
+    }
+}

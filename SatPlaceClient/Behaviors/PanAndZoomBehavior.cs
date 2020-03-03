807bb0b08be07e2fe08dd4cfa64c4cd7b965463d
Jumar Macato <hikari.netto23@gmail.com>
2020-03-03 17:19:31 +0800

    Add pan and zoom behavior

---
+++ SatPlaceClient/Behaviors/PanAndZoomBehavior.cs
@@ -0,0 +1,51 @@
+﻿using Avalonia.Controls.PanAndZoom; 
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity; 
+
+namespace SatPlaceClient.Behaviors
+{
+    public class PanAndZoomBehavior : Behavior<ZoomBorder>
+    {
+        public PanAndZoomBehavior()
+        {
+            HandCursor = new Cursor(StandardCursorType.Hand);
+        }
+
+        private Cursor HandCursor { get; }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyDown += KeyDown;
+            AssociatedObject.PointerEnter += PointerEnter;
+            AssociatedObject.PointerLeave += PointerLeave;
+            AssociatedObject.Uniform();
+        }
+
+        private void PointerLeave(object sender, PointerEventArgs e)
+        {
+            AssociatedObject.Cursor = Cursor.Default;
+        }
+
+        private void PointerEnter(object sender, PointerEventArgs e)
+        {
+            AssociatedObject.Cursor = HandCursor;
+        }
+
+        private void KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.R)
+            {
+                AssociatedObject?.Reset();
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.KeyDown -= KeyDown;
+            AssociatedObject.PointerEnter -= PointerEnter;
+            AssociatedObject.PointerLeave -= PointerLeave;
+        }
+    }
+}

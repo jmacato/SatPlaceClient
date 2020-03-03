eeafe0e6855961132dbb9747630ec05f5e0d64cf
Jumar Macato <hikari.netto23@gmail.com>
2020-03-03 11:52:06 +0800

    Add pixel models

---
+++ SatPlaceClient/Models/GenericPixel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Text;
+using ReactiveUI;
+
+namespace SatPlaceClient.Models
+{
+    public struct GenericPixel
+    {
+        public ushort A;
+        public ushort R;
+        public ushort G;
+        public ushort B;
+
+        public GenericPixel(ushort a, ushort r, ushort g, ushort b)
+        {
+            A = a;
+            R = r;
+            G = g;
+            B = b;
+        }
+    }
+}
---
+++ SatPlaceClient/Models/Json/PixelResult.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json; 
+
+namespace SatPlaceClient.Models.Json
+{
+    public class PixelResult
+    {
+        [JsonProperty("data")]
+        public string DataBase64 { get; set; }
+    }
+}

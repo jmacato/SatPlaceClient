9ca5afa38c93c5d587cdb7b4c0d0fd0c148e135b
Jumar Macato <hikari.netto23@gmail.com>
2020-03-04 14:26:12 +0800

    add rgb normalization functions

--- SatPlaceClient/Models/GenericPixel.cs
+++ SatPlaceClient/Models/GenericPixel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Reactive.Disposables;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -8,7 +9,7 @@ using ReactiveUI;
 namespace SatPlaceClient.Models
 {
     [StructLayout(LayoutKind.Explicit)]
-    public readonly struct GenericPixel 
+    public readonly struct GenericPixel
     {
         [FieldOffset(0)]
         public readonly byte B;
@@ -18,12 +19,12 @@ namespace SatPlaceClient.Models
 
         [FieldOffset(2)]
         public readonly byte R;
-        
+
         [FieldOffset(3)]
         public readonly byte A;
 
         /// <summary>
-        /// A struct that represents a ARGB color and is aligned as
+        /// A struct that represents an ARGB color and is aligned as
         /// a BGRA bytefield in memory.
         /// </summary>
         /// <param name="r">Red</param>
@@ -37,6 +38,24 @@ namespace SatPlaceClient.Models
             this.G = g;
             this.B = b;
         }
- 
+
+        public static GenericPixel FromVector(Vector4 rgba)
+        {
+            var nR = (byte)(rgba.X * byte.MaxValue);
+            var nG = (byte)(rgba.Y * byte.MaxValue);
+            var nB = (byte)(rgba.Z * byte.MaxValue);
+            var nA = (byte)(rgba.W * byte.MaxValue);
+            return new GenericPixel(nA, nR, nG, nB);
+        }
+
+        public Vector4 ToVector()
+        {
+            var nR = (R / 255f);
+            var nG = (G / 255f);
+            var nB = (B / 255f);
+            var nA = (A / 255f);
+
+            return new Vector4(nR, nG, nB, nA);
+        }
     }
 }

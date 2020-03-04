f69a0a8b61ccdc0bf8d861b1210d977b88c1306d
Jumar Macato <hikari.netto23@gmail.com>
2020-03-04 22:37:58 +0800

    Move out alternative delta-e comparators

--- SatPlaceClient/ViewModels/MainWindowViewModel.cs
+++ SatPlaceClient/ViewModels/MainWindowViewModel.cs
@@ -191,31 +191,7 @@ namespace SatPlaceClient.ViewModels
 
             LatestCanvasBitmap = newTargetImage;
         }
-
-        private double AdobeLumaColorCompare(IColorSpace c1, IColorSpace c2)
-        {
-            var rgb1 = c1.ToRgb();
-            var rgb2 = c2.ToRgb();
-
-            double luma1 = (rgb1.R * 299 + rgb1.G * 587 + rgb1.B * 114) / (255.0 * 1000);
-            double luma2 = (rgb2.R * 299 + rgb2.G * 587 + rgb2.B * 114) / (255.0 * 1000);
-            double lumadiff = luma1 - luma2;
-            double diffR = (rgb1.R - rgb2.R) / 255.0, diffG = (rgb1.G - rgb2.G) / 255.0, diffB = (rgb1.B - rgb2.B) / 255.0;
-            return (diffR * diffR * 0.299 + diffG * diffG * 0.587 + diffB * diffB * 0.114) * 0.75
-                 + lumadiff * lumadiff;
-        }
-        private double CompuPhaseColorCompare(IColorSpace c1, IColorSpace c2)
-        {
-            var rgb1 = c1.ToRgb();
-            var rgb2 = c2.ToRgb();
-
-            long rmean = ((long)rgb1.R + (long)rgb2.R) / 2;
-            long r = (long)rgb1.R - (long)rgb2.R;
-            long g = (long)rgb1.G - (long)rgb2.G;
-            long b = (long)rgb1.B - (long)rgb2.B;
-            return Math.Sqrt((((512 + rmean) * r * r) >> 8) + 4 * g * g + (((767 - rmean) * b * b) >> 8));
-        }
-
+ 
         private async void DoTryConnecting()
         {
             try

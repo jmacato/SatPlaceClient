2c4efd65820a0fb8f5fd1b8e2a477fbd33e9e184
Jumar Macato <hikari.netto23@gmail.com>
2020-03-04 19:46:16 +0800

    remove unneeded colorspaces

--- SatPlaceClient/Modules/ColorMinePortable/ColorSpaces.cs
+++ SatPlaceClient/Modules/ColorMinePortable/ColorSpaces.cs
@@ -1,27 +1,16 @@
-﻿
-
-
-
-
-
-
-
-
-//Note: This is a generated file.
+﻿//Note: This is a generated file.
 using SatPlaceClient.ImageProcessing.Conversions;
 
 namespace SatPlaceClient.ImageProcessing
 {
-
-	public interface IRgb : IColorSpace
+    public interface IRgb : IColorSpace
     {
 
-		double R { get; set; }
+        double R { get; set; }
 
-		double G { get; set; }
+        double G { get; set; }
 
-		double B { get; set; }
-
+        double B { get; set; }
     }
 
     public class Rgb : ColorSpace, IRgb
@@ -39,14 +28,13 @@ namespace SatPlaceClient.ImageProcessing
 
         public double R { get; set; }
 
-		public double G { get; set; }
+        public double G { get; set; }
 
-		public double B { get; set; }
-
+        public double B { get; set; }
 
         public override void Initialize(IRgb color)
         {
-            RgbConverter.ToColorSpace(color,this);
+            RgbConverter.ToColorSpace(color, this);
         }
 
         public override IRgb ToRgb()
@@ -55,31 +43,30 @@ namespace SatPlaceClient.ImageProcessing
         }
     }
 
-
-	public interface IXyz : IColorSpace
+    public interface IXyz : IColorSpace
     {
 
-		double X { get; set; }
+        double X { get; set; }
 
-		double Y { get; set; }
+        double Y { get; set; }
 
-		double Z { get; set; }
+        double Z { get; set; }
 
     }
 
     public class Xyz : ColorSpace, IXyz
     {
 
-		public double X { get; set; }
+        public double X { get; set; }
 
-		public double Y { get; set; }
+        public double Y { get; set; }
 
-		public double Z { get; set; }
+        public double Z { get; set; }
 
 
         public override void Initialize(IRgb color)
         {
-            XyzConverter.ToColorSpace(color,this);
+            XyzConverter.ToColorSpace(color, this);
         }
 
         public override IRgb ToRgb()
@@ -88,64 +75,41 @@ namespace SatPlaceClient.ImageProcessing
         }
     }
 
-
-	public interface IHsl : IColorSpace
+    public interface IHsl : IColorSpace
     {
 
-		double H { get; set; }
+        double H { get; set; }
 
-		double S { get; set; }
+        double S { get; set; }
 
-		double L { get; set; }
+        double L { get; set; }
 
     }
-
-    public class Hsl : ColorSpace, IHsl
+ 
+    public interface ILab : IColorSpace
     {
 
-		public double H { get; set; }
+        double L { get; set; }
 
-		public double S { get; set; }
+        double A { get; set; }
 
-		public double L { get; set; }
-
-
-        public override void Initialize(IRgb color)
-        {
-            HslConverter.ToColorSpace(color,this);
-        }
-
-        public override IRgb ToRgb()
-        {
-            return HslConverter.ToColor(this);
-        }
-    }
-
-
-	public interface ILab : IColorSpace
-    {
-
-		double L { get; set; }
-
-		double A { get; set; }
-
-		double B { get; set; }
+        double B { get; set; }
 
     }
 
     public class Lab : ColorSpace, ILab
     {
 
-		public double L { get; set; }
+        public double L { get; set; }
 
-		public double A { get; set; }
+        public double A { get; set; }
 
-		public double B { get; set; }
+        public double B { get; set; }
 
 
         public override void Initialize(IRgb color)
         {
-            LabConverter.ToColorSpace(color,this);
+            LabConverter.ToColorSpace(color, this);
         }
 
         public override IRgb ToRgb()
@@ -154,31 +118,30 @@ namespace SatPlaceClient.ImageProcessing
         }
     }
 
-
-	public interface ILch : IColorSpace
+    public interface ILch : IColorSpace
     {
 
-		double L { get; set; }
+        double L { get; set; }
 
-		double C { get; set; }
+        double C { get; set; }
 
-		double H { get; set; }
+        double H { get; set; }
 
     }
 
     public class Lch : ColorSpace, ILch
     {
 
-		public double L { get; set; }
+        public double L { get; set; }
 
-		public double C { get; set; }
+        public double C { get; set; }
 
-		public double H { get; set; }
+        public double H { get; set; }
 
 
         public override void Initialize(IRgb color)
         {
-            LchConverter.ToColorSpace(color,this);
+            LchConverter.ToColorSpace(color, this);
         }
 
         public override IRgb ToRgb()
@@ -188,30 +151,30 @@ namespace SatPlaceClient.ImageProcessing
     }
 
 
-	public interface ILuv : IColorSpace
+    public interface ILuv : IColorSpace
     {
 
-		double L { get; set; }
+        double L { get; set; }
 
-		double U { get; set; }
+        double U { get; set; }
 
-		double V { get; set; }
+        double V { get; set; }
 
     }
 
     public class Luv : ColorSpace, ILuv
     {
 
-		public double L { get; set; }
+        public double L { get; set; }
 
-		public double U { get; set; }
+        public double U { get; set; }
 
-		public double V { get; set; }
+        public double V { get; set; }
 
 
         public override void Initialize(IRgb color)
         {
-            LuvConverter.ToColorSpace(color,this);
+            LuvConverter.ToColorSpace(color, this);
         }
 
         public override IRgb ToRgb()
@@ -221,30 +184,30 @@ namespace SatPlaceClient.ImageProcessing
     }
 
 
-	public interface IYxy : IColorSpace
+    public interface IYxy : IColorSpace
     {
 
-		double Y1 { get; set; }
+        double Y1 { get; set; }
 
-		double X { get; set; }
+        double X { get; set; }
 
-		double Y2 { get; set; }
+        double Y2 { get; set; }
 
     }
 
     public class Yxy : ColorSpace, IYxy
     {
 
-		public double Y1 { get; set; }
+        public double Y1 { get; set; }
 
-		public double X { get; set; }
+        public double X { get; set; }
 
-		public double Y2 { get; set; }
+        public double Y2 { get; set; }
 
 
         public override void Initialize(IRgb color)
         {
-            YxyConverter.ToColorSpace(color,this);
+            YxyConverter.ToColorSpace(color, this);
         }
 
         public override IRgb ToRgb()
@@ -252,175 +215,4 @@ namespace SatPlaceClient.ImageProcessing
             return YxyConverter.ToColor(this);
         }
     }
-
-
-	public interface ICmy : IColorSpace
-    {
-
-		double C { get; set; }
-
-		double M { get; set; }
-
-		double Y { get; set; }
-
-    }
-
-    public class Cmy : ColorSpace, ICmy
-    {
-
-		public double C { get; set; }
-
-		public double M { get; set; }
-
-		public double Y { get; set; }
-
-
-        public override void Initialize(IRgb color)
-        {
-            CmyConverter.ToColorSpace(color,this);
-        }
-
-        public override IRgb ToRgb()
-        {
-            return CmyConverter.ToColor(this);
-        }
-    }
-
-
-	public interface ICmyk : IColorSpace
-    {
-
-		double C { get; set; }
-
-		double M { get; set; }
-
-		double Y { get; set; }
-
-		double K { get; set; }
-
-    }
-
-    public class Cmyk : ColorSpace, ICmyk
-    {
-
-		public double C { get; set; }
-
-		public double M { get; set; }
-
-		public double Y { get; set; }
-
-		public double K { get; set; }
-
-
-        public override void Initialize(IRgb color)
-        {
-            CmykConverter.ToColorSpace(color,this);
-        }
-
-        public override IRgb ToRgb()
-        {
-            return CmykConverter.ToColor(this);
-        }
-    }
-
-
-	public interface IHsv : IColorSpace
-    {
-
-		double H { get; set; }
-
-		double S { get; set; }
-
-		double V { get; set; }
-
-    }
-
-    public class Hsv : ColorSpace, IHsv
-    {
-
-		public double H { get; set; }
-
-		public double S { get; set; }
-
-		public double V { get; set; }
-
-
-        public override void Initialize(IRgb color)
-        {
-            HsvConverter.ToColorSpace(color,this);
-        }
-
-        public override IRgb ToRgb()
-        {
-            return HsvConverter.ToColor(this);
-        }
-    }
-
-
-	public interface IHsb : IColorSpace
-    {
-
-		double H { get; set; }
-
-		double S { get; set; }
-
-		double B { get; set; }
-
-    }
-
-    public class Hsb : ColorSpace, IHsb
-    {
-
-		public double H { get; set; }
-
-		public double S { get; set; }
-
-		public double B { get; set; }
-
-
-        public override void Initialize(IRgb color)
-        {
-            HsbConverter.ToColorSpace(color,this);
-        }
-
-        public override IRgb ToRgb()
-        {
-            return HsbConverter.ToColor(this);
-        }
-    }
-
-
-	public interface IHunterLab : IColorSpace
-    {
-
-		double L { get; set; }
-
-		double A { get; set; }
-
-		double B { get; set; }
-
-    }
-
-    public class HunterLab : ColorSpace, IHunterLab
-    {
-
-		public double L { get; set; }
-
-		public double A { get; set; }
-
-		public double B { get; set; }
-
-
-        public override void Initialize(IRgb color)
-        {
-            HunterLabConverter.ToColorSpace(color,this);
-        }
-
-        public override IRgb ToRgb()
-        {
-            return HunterLabConverter.ToColor(this);
-        }
-    }
-
-
 }

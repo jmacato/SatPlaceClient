f100b38679b4f8153c8bf9e6925113c57489ddd2
Jumar Macato <hikari.netto23@gmail.com>
2020-03-05 12:56:27 +0800

    Adjust pattern dithering

--- ImageProcessing/Dithering/AdobePatternDithering.cs
+++ ImageProcessing/Dithering/AdobePatternDithering.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
-using System.Numerics; 
+using System.Numerics;
+using System.Threading.Tasks;
 using SatPlaceClient.Models;
 
 namespace SatPlaceClient.ImageProcessing.Dithering
@@ -23,31 +24,51 @@ namespace SatPlaceClient.ImageProcessing.Dithering
     /// </summary>
     public class AdobePatternDithering
     {
-        /* 8x8 threshold map (note: the patented pattern dithering algorithm uses 4x4) */
-        private static int[] ThresholdMap = {
-            0,48,12,60, 3,51,15,63,
-            32,16,44,28,35,19,47,31,
-            8,56, 4,52,11,59, 7,55,
-            40,24,36,20,43,27,39,23,
-            2,50,14,62, 1,49,13,61,
-            34,18,46,30,33,17,45,29,
-            10,58, 6,54, 9,57, 5,53,
-            42,26,38,22,41,25,37,21 };
- 
-        private Vector3[] allowedPalette;
-        double[] paletteLuminance = new double[16];
-
-        private double ColorCompare(Vector3 rgb1, Vector3 rgb2)
+        private static int[] ThresholdMap =
         {
-            double luma1 = (rgb1.X * 0.299 + rgb1.Y * 0.587 + rgb1.Z * 0.114);
-            double luma2 = (rgb2.X * 0.299 + rgb2.Y * 0.587 + rgb2.Z * 0.114);
-            double lumadiff = luma1 - luma2;
-            double diffR = rgb1.X - rgb2.X,
-                   diffG = rgb1.Y - rgb2.Y,
-                   diffB = rgb1.Z - rgb2.Z;
+             0, 48, 12, 60,  3, 51, 15, 63,
+            32, 16, 44, 28, 35, 19, 47, 31,
+             8, 56,  4, 52, 11, 59,  7, 55,
+            40, 24, 36, 20, 43, 27, 39, 23,
+             2, 50, 14, 62,  1, 49, 13, 61,
+            34, 18, 46, 30, 33, 17, 45, 29,
+            10, 58,  6, 54,  9, 57,  5, 53,
+            42, 26, 38, 22, 41, 25, 37, 21
+        };
 
-            return (diffR * diffR * 0.299 + diffG * diffG * 0.587 + diffB * diffB * 0.114) * 0.75
-                 + lumadiff * lumadiff;
+        private Vector3[] _palette;
+        private float[] _paletteLuminance;
+        private readonly Vector3 _errMult = new Vector3(0.09f, 0.09f, 0.09f);
+        private readonly Vector3 _luminanceCoef = new Vector3(0.299f, 0.587f, 0.114f);
+
+        public float GetLuminance(Vector3 color) => (color * _luminanceCoef).Length();
+
+        public void Dither(int w, int h, ref GenericColor[] pixels, GenericColor[] palette)
+        {
+            this._palette = palette.Select(x => x.ToVector3())
+                                   .ToArray();
+
+            this._paletteLuminance = _palette.Select(x => GetLuminance(x))
+                                             .ToArray();
+
+            int CoordsToIndex(int x, int y) => x + w * y;
+
+            var rePix = pixels;
+
+            Parallel.For(0, h, (y) =>
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    var i = CoordsToIndex(x, y);
+                    var color = rePix[i];
+                    var map_value = ThresholdMap[(x & 7) + ((y & 7) << 3)];
+                    var plan = DeviseBestMixingPlan(color);
+
+                    rePix[i] = palette[plan[map_value]];
+                }
+            });
+
+            pixels = rePix;
         }
 
         private int[] DeviseBestMixingPlan(GenericColor color)
@@ -55,67 +76,53 @@ namespace SatPlaceClient.ImageProcessing.Dithering
             var result = new int[64];
             var src = color.ToVector3();
 
-            var errMult = new Vector3(0.09f, 0.09f, 0.09f);  // Error multiplier
-            var errAcc = new Vector3(); // Error accumulator
+            // Error accumulator
+            var errAcc = new Vector3();
 
-            for (int i = 0; i < 64; ++i)
+            for (int i = 0; i < 64; i++)
             {
                 // Current temporary value
-                var t = src + errAcc * errMult;
+                var temp = src + errAcc * _errMult;
 
                 // Clamp it in the allowed RGB range
-                t = Vector3.Clamp(t, Vector3.Zero, Vector3.One);
+                temp = Vector3.Clamp(temp, Vector3.Zero, Vector3.One);
 
                 // Find the closest color from the palette
-                var least_penalty = double.MaxValue;
-                var chosen = i % 16;
+                var leastPenalty = float.MaxValue;
+                var chosenIndex = i % 16;
 
                 for (int j = 0; j < 16; j++)
                 {
-                    var chosenColor = allowedPalette[j];
-                    var penalty = ColorCompare(chosenColor, t);
+                    var chosenColor = _palette[j];
+                    var penalty = ColorCompare(chosenColor, temp);
 
-                    if (penalty < least_penalty)
+                    if (penalty < leastPenalty)
                     {
-                        least_penalty = penalty;
-                        chosen = j;
+                        leastPenalty = penalty;
+                        chosenIndex = j;
                     }
                 }
+
                 // Add it to candidates and update the error
-                result[i] = chosen;
-                errAcc += src - allowedPalette[chosen];
+                result[i] = chosenIndex;
+                errAcc += src - _palette[chosenIndex];
             }
 
             // Sort the colors according to luminance
-            return result.OrderBy(x => paletteLuminance[x]).ToArray();
+            return result.OrderBy(x => _paletteLuminance[x]).ToArray();
         }
 
-        public void Dither(int w, int h, ref GenericColor[] pixels, GenericColor[] palette)
+        private float ColorCompare(Vector3 rgb1, Vector3 rgb2)
         {
-            this.allowedPalette = palette.Select(x => x.ToVector3()).ToArray();
+            var l1 = GetLuminance(rgb1);
+            var l2 = GetLuminance(rgb2);
+            var deltaL = l1 - l2;
 
-            for (int c = 0; c < 16; ++c)
-            {
-                var pal = palette[c].ToVector3();
-                paletteLuminance[c] = pal.X * 0.299f + pal.Y * 0.587f + pal.Z * 0.114f;
-            }
-
-            int CoordsToIndex(int x, int y) => x + w * y;
-
-            for (int y = 0; y < h; ++y)
-            {
-                for (int x = 0; x < w; ++x)
-                {
-                    var i = CoordsToIndex(x, y);
-                    GenericColor color = pixels[i];
-
-                    int map_value = ThresholdMap[(x & 7) + ((y & 7) << 3)];
-
-                    var plan = DeviseBestMixingPlan(color);
-
-                    pixels[i] = palette[plan[map_value]];
-                }
-            }
+            float diffR = rgb1.X - rgb2.X,
+                  diffG = rgb1.Y - rgb2.Y,
+                  diffB = rgb1.Z - rgb2.Z;
+            
+            return (diffR * diffR * _luminanceCoef.X + diffG * diffG * _luminanceCoef.Y + diffB * diffB * _luminanceCoef.Z) * 0.75f + deltaL * deltaL;
         }
     }
 }

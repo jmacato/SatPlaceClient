9f374a6e538423d5198343e55b4a574653e0dd07
Jumar Macato <hikari.netto23@gmail.com>
2020-03-06 00:47:43 +0800

    add longer timeout

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -23,9 +23,9 @@ namespace SatPlaceClient.ViewModels
     {
         public MainWindowViewModel()
         {
-            SatPlaceClient = new SocketIO("https://satoshis-place-api.koala.casa/")
+            SatPlaceClient = new SocketIO("https://api.satoshis.place/")
             {
-
+                ConnectTimeout = TimeSpan.FromMinutes(5)
             };
 
             DoTryConnecting();
